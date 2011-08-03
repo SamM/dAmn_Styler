c8c1f5b45d35eb69fe4d91ff0d9a70870a91fc59
Sam Mulqueen <sammulqueen.nz@gmail.com>
2011-08-03 12:39:54 +0000

    Fixed userscript

--- dAmn_Styler.user.js
+++ dAmn_Styler.user.js
@@ -9,12 +9,19 @@
 var Script_Request = Script_Request || function(id, url, query, parent){
 	query = query || {};
 	query['_random'] = Math.round(Math.random()*10000000).toString();
-	if(el = document.getElementById(id)) el.parentNode.removeChild(el);
-	with(S = document.createElement('script')){
-		if(id!=null)S.id = id; S.type = 'text/javascript'; 
-		S.src = url + (function(q){ var out = []; for(var k in q) out.push([k,q[k]].join('=')); return '?'+out.join('&'); })(query);
-		(parent||document.body).appendChild(S);
-		var self = function(onload_callback){ S.onload = function(){ onload_callback(S); }; };
-		return self; }};
+    
+    var el = document.getElementById(id);
+	if(el) el.parentNode.removeChild(el);
+    
+    var S = document.createElement('script');
+	if(id!=null) S.id = id;
+    S.type = 'text/javascript'; 
+	S.src = url + (function(q){ var out = []; for(var k in q) out.push([k,q[k]].join('=')); return '?'+out.join('&'); })(query);
+    
+	parent = parent || document.body;
+    parent.appendChild(S);
+    
+	return function(onload_callback){ S.onload = function(){ onload_callback(S); }; };
+};
 		
 var load_styler = Script_Request('dAmn_Styler', 'http://github.com/SamM/dAmn_Styler/raw/master/dAmn_Styler.js');
